a02f821ba72c5adb1292ca5b56be2e7e65ff75d6
Yan Khonski <yankhonskiy@gmail.com>
2025-06-08 22:23:46 +0200

    Initial commit

---
+++ src/main/java/com/yk/utils/java17pmd/app/DemoApp.java
@@ -0,0 +1,38 @@
+package com.yk.utils.java17pmd.app;
+
+import com.yk.utils.java17pmd.app.core.SomeClass;
+import com.yk.utils.java17pmd.app.utils.NumberUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// UnusedImports
+//  import java.util.ArrayList;
+
+public class DemoApp {
+
+  private static final Logger LOGGER = LogManager.getLogger(DemoApp.class);
+
+  public static void main(String[] args) {
+    // a - ShortVariable
+    int aVariable = 5;
+
+    @SuppressWarnings("PMD.ShortVariable") // Here PMD ignores short name.
+    int b = 6;
+
+    int cResult = aVariable + b;
+    LOGGER.info("c: {}", cResult);
+    int id = 15; // id name is allowed by exception
+    LOGGER.info("id: {}", id);
+
+    // SystemPrintln
+    // System.out.println("Using system out println");
+
+    // SomeClass someClass = new SomeClass(); - LocalVariableCouldBeFinal
+    SomeClass someClass = new SomeClass();
+    LOGGER.info("someClass: {}", someClass);
+
+    int number = 10;
+    String convertedNumber = NumberUtils.convertNumberToString(number);
+    LOGGER.info("Converted number: {} is string: {}.", number, convertedNumber);
+  }
+}
